6dc56cba7a0c90536a10573c1261413392c66aab
Alberto García <agarciablasco2@gmail.com>
2023-04-28 22:37:09 -0400

    Terminación M4 - 2.1
    Afinación de métodos y comentarios explicativos

--- m4_individual2.1/main.py
+++ m4_individual2.1/main.py
@@ -7,7 +7,7 @@ class Piloto:
         self.__estres = 0
         self.__ahorro = 0
         self.en_coche = False
-        
+    
     def subir_al_coche(self):
         self.en_coche = True
     
@@ -20,14 +20,13 @@ class Piloto:
     def defender(self):
         pass
     
+    # Con este método modificaríamos la velocidad con la que disminuye el combustible del vehículo
     def modo_ahorro(self, ahorro):
-        self.ahorro = ahorro
+        self.__ahorro = ahorro
     
+    # Modifica el estrés del piloto en relación a diversos factores, pudiendo cometer errores o provocando rotura del vehículo
     def modifica_estres(self, estres):
-        self.ahorro = estres
-    
-    def entra_boxes(self):
-        pass
+        self.__estres += estres
 
 class Equipo:
     def __init__(self, nombre, pilotos, coche):
@@ -35,9 +34,13 @@ class Equipo:
         self.pilotos = pilotos
         self.coche = coche
     
+    # El coche es llamado a boxes para realizar las operaciones pertinentes, cambio de ruedas, respostaje, etc
     def llama_box(self):
         coche.en_box = True
     
+    def saca_a_pista(self):
+        coche.en_box = False
+
     # Método "sobrecargado"
     def cambia_piloto(self, piloto):
         if coche.piloto is None:
@@ -85,7 +88,7 @@ class Circuito:
         self.lluvia = lluvia
     
 class Coche:
-    def __init__(self, modelo, potencia, peso, piloto=None):
+    def __init__(self, modelo, potencia, peso, aceleracion, vel_maxima, pot_frenos, piloto=None):
         self.modelo = modelo
         self.potencia = potencia
         self.peso = peso
@@ -93,14 +96,17 @@ class Coche:
         self.en_box = True
         self.combustible = 100
         self.velocidad = 0
+        self.aceleracion = aceleracion
+        self.vel_maxima = vel_maxima
+        self.pot_frenos = pot_frenos
         self.flashing_lights = False
         self.piloto = None
     
     def acelerar(self):
-        pass
+        self.velocidad += self.aceleracion
     
     def frenar(self):
-        pass
+        self.velocidad -= self.pot_frenos
     
     def flash_luces(self):
         self.flashing_lights = True
@@ -116,11 +122,21 @@ class Coche:
     def rotura_aleatoria(self):
         pass
     
-coche = Coche('Peugeot 905', 670, 750)
+# Instancian objetos coche, piloto y equipo
+coche = Coche('Peugeot 905', 670, 750, 40, 392, 65)
 piloto1 = Piloto('Thierry Boutsen', 45, 85)
 piloto2 = Piloto('Mark Blundell', 51, 76)
 equipo = Equipo('Peugeot Talbot Sport', [piloto1, piloto2], coche)
+
+# Se saca coche a pista y línea de print para debug
 equipo.cambia_piloto(equipo.pilotos[0])
+equipo.saca_a_pista()
+print(equipo.coche.en_box)
+# Se realiza un cambio de piloto y línea de print para debug
+equipo.llama_box()
+print(equipo.coche.en_box)
 print(equipo.coche.piloto.nombre)
 equipo.cambia_piloto(equipo.pilotos[1])
 print(equipo.coche.piloto.nombre)
+equipo.saca_a_pista()
+print(equipo.coche.en_box)
